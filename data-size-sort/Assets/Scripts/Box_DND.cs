c386bba1d222768eddb54da9bfee6460c95dc969
Jake Schichko <schichko@udel.edu>
2019-01-15 05:24:01 +1300

    Changes

---
+++ data-size-sort/Assets/Scripts/Box_DND.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.EventSystems;
+
+
+public class Box_DND : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+
+
+{
+    int initialPointerId;
+
+    void Start()
+    {
+        initialPointerId = int.MaxValue;
+    }
+
+    void OnBeginDrag(PointerEventData eventData)
+    {
+        if (initialPointerId == int.MaxValue)
+        {
+            initialPointerId = eventData.pointerId;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (initialPointerId == eventData.pointerId)
+        {
+            initialPointerId = int.MaxValue;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (initialPointerId == eventData.pointerId)
+        {
+            // DO WHATEVER YOU WANNA DO DURING DRAGGING HERE
+        }
+    }
+}
