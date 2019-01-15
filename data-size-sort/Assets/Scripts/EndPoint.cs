627e71828dd14378989675d6469d8515f0e66c75
channum <charlie.hannum@gmail.com>
2019-01-15 21:47:18 +1300

    Changed system for progress checking; made scenes loop

---
+++ data-size-sort/Assets/Scripts/EndPoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndPoint : MonoBehaviour
+{
+    private bool full;
+
+    private void Start()
+    {
+        full = false;
+    }
+
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        full = true;
+    }
+
+    public bool isFull()
+    {
+        return full;
+    }
+}
