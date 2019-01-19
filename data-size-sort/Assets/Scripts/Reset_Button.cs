9a4aca979b95fc8d95f40b3fe0339fa07ef74b63
channum <charlie.hannum@gmail.com>
2019-01-19 20:13:20 +1300

    Added reset button functionality

--- data-size-sort/Assets/Scripts/Reset_Button.cs
+++ data-size-sort/Assets/Scripts/Reset_Button.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Reset_Button : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        
-    }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
 
-   
-    void onMouseUpAsButton()
-    {
-        Debug.Log("Text: ");
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (hit.collider != null && hit.collider.tag == "Reset")
+            {
+                Debug.Log("Level Reset");
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
     }
+    
 }
