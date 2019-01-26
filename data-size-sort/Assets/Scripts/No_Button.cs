b690159460a8d6fd6a5140b7b5195bb4ca223db9
Erin O'Connor <erino@udel.edu>
2019-01-26 20:20:38 +1300

    working end scene buttons

---
+++ data-size-sort/Assets/Scripts/No_Button.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class No_Button : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Exits the game if pressed
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif     
+        }
+    }
+}
---
+++ data-size-sort/Assets/Scripts/Yes_Button.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Yes_Button : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Changes back to original level 1 scene
+            SceneManager.LoadScene("Level1", LoadSceneMode.Single);
+        }
+    }
+}
