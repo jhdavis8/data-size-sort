6b6dd688864337e68b18cf509ababf81e3a0acbc
Josh Davis <jhdavis@udel.edu>
2019-01-29 09:43:04 +1300

    End scene messages, aesthetic changes, progress on 4K res stuff

--- data-size-sort/Assets/Scripts/GameController.cs
+++ data-size-sort/Assets/Scripts/GameController.cs
@@ -10,18 +10,25 @@ public class GameController : MonoBehaviour
 {
     public Box[] boxes;
     public GUISkin skin;
+    public GUISkin bigSkin;
     private bool complete;
-    private bool end_message_finished = true;
     private bool show_end_message = true;
     private Box somethingHeld;
     private bool intro1_complete = false;
     private bool intro2_complete = false;
+    private Rect boxDefault = new Rect(600, 100, 650, 450);
+    private Rect boxBig = new Rect(600, 100, 650, 450);
+    private Rect buttonDefault = new Rect(875, 375, 100, 50);
+    private Rect buttonBig = new Rect(875, 375, 100, 50);
+    private Rect boxUsed;
+    private Rect buttonUsed;
     public Check checkBox;
     public EndPoint[] endPoints;
 
     private void Start()
     {
         somethingHeld = null;
+        Debug.Log(Screen.currentResolution);
     }
 
     /*
@@ -30,6 +37,7 @@ public class GameController : MonoBehaviour
     void Update()
     {
         complete = true;
+        show_end_message = false;
         for (int i = 0; i < boxes.Length; i++)
         {
             if (!boxes[i].CorrectDrop())
@@ -44,11 +52,6 @@ public class GameController : MonoBehaviour
             {
                 show_end_message = true;
                 Debug.Log("Complete");
-                if (end_message_finished)
-                {
-                    SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1) % 3);
-                }
-                
             }
             else
             {
@@ -76,33 +79,53 @@ public class GameController : MonoBehaviour
 
     void OnGUI()
     {
-        GUI.skin = skin;
+        if (Screen.currentResolution.ToString().Contains("3840"))
+        {
+            GUI.skin = bigSkin;
+            boxUsed = boxBig;
+            buttonUsed = buttonBig;
+        }   
+        else
+        {
+            GUI.skin = skin;
+            boxUsed = boxDefault;
+            buttonUsed = buttonDefault;
+        }
         Debug.Log(SceneManager.GetActiveScene().name);
         if (!intro1_complete && SceneManager.GetActiveScene().name == "Level1")
         {
-            GUI.Box(new Rect(100, 100, 600, 400),
+            GUI.Box(boxUsed,
                 "Welcome to Data Size Sort. In this level, you need to order the various data size labels from smallest to largest." +
                 "\n -- You can pick up a label by clicking on it and dragging." +
                 "\n -- Release each box into the black end points to sort them. " +
                 "\n -- Click check when you have finished to see if you are correct" +
                 "\n -- Click hint to see a hint. " +
                 "\n -- Click reset to send all the labels to their starting positions.");
-            if (GUI.Button(new Rect(350, 350, 100, 50), "OK"))
+            if (GUI.Button(buttonUsed, "OK"))
             {
                 intro1_complete = true;
             }
         }
         else if (!intro2_complete && SceneManager.GetActiveScene().name == "Level2")
         {
-            GUI.Box(new Rect(100, 100, 600, 400),
+            GUI.Box(boxUsed,
                 "Now, categorize these various real-world examples of data sizes by which data size you think they best fit under." +
                 "\n -- You can pick up a label by clicking on it and dragging." +
                 "\n -- Release each box into the black end points to categorize them. " +
                 "\n -- Click check when you have finished to see if you are correct." +
                 "\n -- Click return to send all the labels to their starting positions.");
-            if (GUI.Button(new Rect(350, 350, 100, 50), "OK"))
+            if (GUI.Button(buttonUsed, "OK"))
             {
                 intro2_complete = true;
+            }
+        }
+        else if (show_end_message)
+        {
+            GUI.Box(boxUsed,
+                "You have completed the level. Congratulations!");
+            if (GUI.Button(buttonUsed, "OK"))
+            {
+                SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1) % 3);
             }
         }
     }
